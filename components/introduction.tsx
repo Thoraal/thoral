2314eb062a9ea9021119635b54f5a07a7235e28a
Thooral <alejandro.toral.12@gmail.com>
2024-11-10 20:26:37 -0500

    main

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -38,7 +38,7 @@ const Introduction = () => {
                         target="_blank"
                         rel="noopener noreferrer"
                         className="px-3 py-2 my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary">
-                        Contactame
+                        Contactame 📱
                         </a>
 
                     </div>
