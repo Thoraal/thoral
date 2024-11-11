3a7e8fa1840fd09ab6dc70f3c261815c66299866
Thooral <alejandro.toral.12@gmail.com>
2024-11-10 20:24:30 -0500

    main

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -4,8 +4,8 @@ import { TypeAnimation } from 'react-type-animation';
 const Introduction = () => {
     return (
         <div className="z-20 w-full bg-darkBg/60">
-            <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2 my-4">
-            <Image src="/home-4.png" priority layout="intrinsic"  width={200} height={200} alt="Avatar" className="mx-auto md:w-[330px] md:h-[330px]"/>
+            <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2 my-3">
+            <Image src="/home-4.png" priority layout="intrinsic"  width={180} height={180} alt="Avatar" className="mx-auto md:w-[330px] md:h-[330px]"/>
                 <div className="flex flex-col justify-center max-w-md ">
                     <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">Soy Alejandro Toral, <br />
                         <TypeAnimation
