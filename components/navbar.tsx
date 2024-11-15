d9c0e211e9e47182d9d91fe96c22ab8f830f5940
Thooral <alejandro.toral.12@gmail.com>
2024-11-15 12:22:07 -0500

    main

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 
@@ -8,18 +8,18 @@ import { MotionTransition } from "./transition-component";
 import { usePathname } from "next/navigation";
 
 const Navbar = () => {
-    const router = usePathname()
+    const router = usePathname();
 
     return (
         <MotionTransition position="right" className="fixed z-40 flex flex-col items-center justify-center w-full mt-auto h-max bottom-5">
             <nav>
-                <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/15 background-blur-sm">
+                <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-white/20 backdrop-blur-sm">
                     {itemsNavbar.map((item) => (
                         <div
                             key={item.id}
-                            className={`px-6 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary ${router === item.link && 'bg-secondary'}`}
+                            className={`px-6 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary/80 ${router === item.link && 'bg-secondary/80'}`}
                             data-tooltip-target="tooltip-default">
-                            <Link href={item.link}>{item.icon} </Link>
+                            <Link href={item.link}>{item.icon}</Link>
                         </div>
                     ))}
                 </div>
