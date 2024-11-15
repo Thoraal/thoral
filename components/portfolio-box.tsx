25fde55fb88b92bcf6c850fd04c6ad68c85125f3
Thooral <alejandro.toral.12@gmail.com>
2024-11-15 12:04:56 -0500

    main

--- components/portfolio-box.tsx
+++ components/portfolio-box.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { FaGithub, FaDownload } from "react-icons/fa";
 
 interface PortfolioBoxProps {
     data: {
@@ -28,18 +29,18 @@ const PortfolioBox: React.FC<PortfolioBoxProps> = ({ data: { title, image, urlGi
                     href={urlGithub}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="p-2 transition duration-150 rounded-lg bg-slate-500 hover:bg-slate-500/80 text-white flex-1 text-center"
+                    className="p-2 text-sm transition duration-150 rounded-lg bg-slate-500 hover:bg-slate-500/80 text-white flex-1 text-center flex items-center justify-center gap-2 transform hover:scale-110 hover:bg-opacity-90"
                 >
-                    GitHub
+                    <FaGithub /> GitHub
                 </Link>
 
                 <Link
                     href={urlDemo}
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="p-2 transition duration-150 rounded-lg bg-secondary hover:bg-secondary/80 text-white flex-1 text-center"
+                    className="p-2 text-sm transition duration-150 rounded-lg bg-secondary hover:bg-secondary/80 text-white flex-1 text-center flex items-center justify-center gap-2 transform hover:scale-110 hover:bg-opacity-90"
                 >
-                    Descargar
+                    <FaDownload /> Descargar
                 </Link>
             </div>
         </div>
